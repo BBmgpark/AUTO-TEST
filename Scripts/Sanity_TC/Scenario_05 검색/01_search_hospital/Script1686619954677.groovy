31d5f55c6f37db6af50fc5c2aa02bc2de60b87d9
BBmgpark <mgpark@bbros.kr>
2023-06-20 11:16:46 +0900

    [dev] 시나리오5 TC01 수정

--- Scripts/Sanity_TC/Scenario_05 검색/01_search_hospital/Script1686619954677.groovy
+++ Scripts/Sanity_TC/Scenario_05 검색/01_search_hospital/Script1686619954677.groovy
@@ -109,14 +109,11 @@ Mobile.setText(findTestObject('09_search/input_search'), disease[i], GlobalVaria
 'STEP - [검색] 버튼 선택'
 Mobile.tap(findTestObject('09_search/btn_search'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'검색 목록 첫번째 검색어 저장'
-search_list = Mobile.getText(findTestObject('09_search/txt_search_list_first'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
-
 'STEP - 검색 목록 첫번째 검색어 선택'
 Mobile.tap(findTestObject('09_search/btn_result_plus'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 검색 인풋 영역에 검색어 노출'
-Mobile.verifyElementText(findTestObject('09_search/input_search'), search_list, FailureHandling.CONTINUE_ON_FAILURE) 
+'STEP - 검색어 저장'
+search_list = Mobile.getText(findTestObject('09_search/input_search'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
 '기대결과 - 질벙졍보 설명 영역 노출'
 Mobile.verifyElementVisible(findTestObject('09_search/txt_disease_description'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
@@ -173,14 +170,11 @@ Mobile.setText(findTestObject('09_search/input_search'), medical_department[i], 
 'STEP - [검색] 버튼 선택'
 Mobile.tap(findTestObject('09_search/btn_search'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
-'검색 목록 첫번째 검색어 저장'
-search_list = Mobile.getText(findTestObject('09_search/txt_search_list_first'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
-	
 'STEP - 검색 결과 첫번째 검색어 선택'
 Mobile.tap(findTestObject('09_search/btn_result_plus'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
 '기대결과 - 진료과 검색 결과 목록 노출'
-Mobile.verifyElementText(findTestObject('09_search/input_search'), search_list, FailureHandling.CONTINUE_ON_FAILURE) //검색 영역
+Mobile.verifyElementText(findTestObject('09_search/input_search'), medical_department[i], FailureHandling.CONTINUE_ON_FAILURE) //검색 영역
 Mobile.verifyElementVisible(findTestObject('09_search/btn_filter'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[필터] 버튼
 Mobile.verifyElementText(findTestObject('09_search/txt_sort_type'), '관련순', FailureHandling.CONTINUE_ON_FAILURE) //sort 타입
 Mobile.verifyElementVisible(findTestObject('09_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //병원명 영역
@@ -230,14 +224,16 @@ Mobile.verifyElementVisible(findTestObject('09_search/area_naver_map'), GlobalVa
 'STEP - [<-] 뒤로가기 버튼 선택'
 Mobile.tap(findTestObject('00_common/btn_backBtn'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
+Mobile.delay(2)
+
 '기대결과 - 검색 화면으로 이동'
-Mobile.verifyElementText(findTestObject('09_search/input_search'), search_list, FailureHandling.CONTINUE_ON_FAILURE) //검색 영역
+Mobile.verifyElementText(findTestObject('09_search/input_search'), medical_department[i], FailureHandling.CONTINUE_ON_FAILURE) //검색 영역
 	
 'STEP - [x] 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('09_search/btn_search_clear'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
 '기대결과 - 검색어 삭제되고, 최근 검색어 노출'
-Mobile.verifyElementText(findTestObject('09_search/txt_recent_list'), search_list, FailureHandling.CONTINUE_ON_FAILURE) //검색어
+Mobile.verifyElementText(findTestObject('09_search/txt_recent_list'), medical_department[i], FailureHandling.CONTINUE_ON_FAILURE) //검색어
 	
 'STEP - [x] 최근 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('09_search/btn_recent_list_delete'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
