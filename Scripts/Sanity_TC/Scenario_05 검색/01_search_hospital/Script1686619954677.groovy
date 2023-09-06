bb032f0e5997cc73ebe0facaef0cd509611cc60b
BBmgpark <mgpark@bbros.kr>
2023-09-06 12:47:46 +0900

    [dev] 주소설정 #1

--- Scripts/Sanity_TC/Scenario_05 검색/01_search_hospital/Script1686619954677.groovy
+++ Scripts/Sanity_TC/Scenario_05 검색/01_search_hospital/Script1686619954677.groovy
@@ -26,10 +26,14 @@ import io.appium.java_client.MobileElement
 'STEP - 똑닥 앱 실행'
 Mobile.startExistingApplication(GlobalVariable.appid)
 
-'STEP - 홈 검색 영역 선택'
-Mobile.tap(findTestObject('03_home/area_view_search_bar'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+Mobile.delay(2)
 
-'back key (키보드 닫기)'
+'STEP - 홈 검색 영역 선택'
+Mobile.tap(findTestObject('03_home/area_view_search_bar'), 10, FailureHandling.CONTINUE_ON_FAILURE)
+
+Mobile.delay(2)
+
+'STEP - 가상 키보드 닫기'
 Mobile.pressBack()
 
 '최근 검색어 존재 시 전체 삭제'
@@ -38,9 +42,6 @@ if(Mobile.waitForElementPresent(findTestObject('08_search/btn_all_delete'), Glob
 	Mobile.tap(findTestObject('08_search/btn_all_delete'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	Mobile.tap(findTestObject('00_common/btn_positiveTxt'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 }
-
-'기대결과 - 검색 화면 노출'
-Mobile.verifyElementVisible(findTestObject('08_search/input_search'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
 //병원 검색
 'STEP - 검색 영역 선택'
@@ -55,49 +56,52 @@ Mobile.tap(findTestObject('08_search/btn_search'), GlobalVariable.fixedTime, Fai
 'STEP - 검색 결과 첫번째 리스트 [+] 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_result_plus'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 병원 검색 결과 노출'
-Mobile.verifyElementVisible(findTestObject('08_search/btn_filter'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[필터] 버튼
-Mobile.verifyElementText(findTestObject('08_search/txt_sort_type'), '관련순', FailureHandling.CONTINUE_ON_FAILURE) //sort 타입
+'기대결과 - 병원 검색 결과가 노출된다.'
 Mobile.verifyElementText(findTestObject('08_search/txt_hospital_name'), GlobalVariable.hospital_name, FailureHandling.CONTINUE_ON_FAILURE) //병원명
 
-'기대결과 - [진료중] [지도보기] 버튼 노출'
+'기대결과 - 필터 버튼이 노출된다.'
+Mobile.verifyElementText(findTestObject('08_search/txt_sort_type'), '관련순', FailureHandling.CONTINUE_ON_FAILURE) //sort 타입
+
+'기대결과 - [진료중] [지도보기] 버튼이 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/btn_filter_open'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[진료중]
 Mobile.verifyElementVisible(findTestObject('08_search/btn_map'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[지도보기]
-
-'검색 결과 첫번째 병원 이름 저장'
-hospital_name1 = Mobile.getText(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
 'STEP - 병원 목록 선택'
 Mobile.tap(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-//병원상세 요소 못찾아서 우선 제외
-//'기대결과 - 병원상세 화면으로 이동'
-//Mobile.verifyElementVisible(findTestObject('00_common/txt_class_text_view', [('text') : hospital_name1]), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //page title
-//
-//'STEP - [<-] 병원 상세  뒤로가기 버튼 선택'
-//Mobile.tap(findTestObject('00_common/btn_webview_back'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
-Mobile.pressBack()
+Mobile.delay(3)
 
-'기대결과 - 검색 화면으로 이동'
+//'기대결과 - 병원상세로 이동되고, [시간예약] 버튼이 노출된다.'
+//Mobile.verifyElementVisible(findTestObject('09_hospital_detail/btn_right', [('text') : '시간예약']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+//
+//'기대결과 - 병원상세로 이동되고, [바로접수] 버튼이 노출된다.'
+//Mobile.verifyElementVisible(findTestObject('09_hospital_detail/btn_left', [('text') : '바로접수']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+
+'STEP - [<-] 뒤로가기 버튼 선택'
+Mobile.tap(findTestObject('00_common/btn_webview_back'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+
+Mobile.delay(2)
+
+'기대결과 - 검색 화면으로 이동된다.'
 Mobile.verifyElementText(findTestObject('08_search/input_search'), GlobalVariable.hospital_name, FailureHandling.CONTINUE_ON_FAILURE) //검색 인풋 문구
 
 'STEP - [x] 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_search_clear'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 검색어 삭제되고, 최근 검색어 노출' 
+'기대결과 - 검색어 삭제되고 최근 검색어가 노출된다.' 
 Mobile.verifyElementText(findTestObject('08_search/txt_recent_list'), GlobalVariable.hospital_name, FailureHandling.CONTINUE_ON_FAILURE) //최근 검색어
-Mobile.verifyElementVisible(findTestObject('08_search/btn_all_delete'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[전체삭제] 버튼
 
 'STEP - [x] 최근 검색어 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_recent_list_delete'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 삭제한 최근 검색어 미노출'
+'기대결과 - 삭제한 최근 검색어가 노출되지 않는다.'
 Mobile.verifyElementNotVisible(findTestObject('08_search/txt_recent_list'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 //
 
 //질병 검색
 String[] disease = ['감기', '장염']
 
+'STEP - 질병 검색'
 for(i=0; i<=1; i++)
 {
 'STEP - 검색 영역 선택'
@@ -112,40 +116,23 @@ Mobile.tap(findTestObject('08_search/btn_search'), GlobalVariable.fixedTime, Fai
 'STEP - 검색 목록 첫번째 검색어 선택'
 Mobile.tap(findTestObject('08_search/btn_result_plus'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'STEP - 검색어 저장'
-search_list = Mobile.getText(findTestObject('08_search/input_search'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
-
-'기대결과 - 질벙졍보 설명 영역 노출'
+'기대결과 - 질벙졍보 설명 영역이 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_disease_description'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 Mobile.verifyElementVisible(findTestObject('08_search/btn_see'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[보기] 버튼
 
-'기대결과 - 질병 검색 결과 노출'
+'기대결과 - 질병 검색 결과가 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/btn_filter'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //[필터] 버튼
 Mobile.verifyElementText(findTestObject('08_search/txt_sort_type'), '관련순', FailureHandling.CONTINUE_ON_FAILURE) //sort 타입
 Mobile.verifyElementVisible(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //병원명 영역
-
-'검색 결과 목록 첫번째 병원 이름 저장'
-hospital_name2 = Mobile.getText(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
 'STEP - 병원 목록 선택'
 Mobile.tap(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-//병원상세 요소 못찾아서 보류
-//'기대결과 - 병원상세 화면으로 이동'
-//Mobile.verifyElementVisible(findTestObject('00_common/txt_class_text_view', [('text') : hospital_name2]), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //page title
-//
-//'STEP - [<-] 병원상세 뒤로가기 버튼 선택'
-//Mobile.tap(findTestObject('00_common/btn_webview_back'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+'STE[ - 이전 화면으로 이동'
 Mobile.pressBack()
-
-'기대결과 - 검색 화면으로 이동'
-Mobile.verifyElementText(findTestObject('08_search/input_search'), search_list, FailureHandling.CONTINUE_ON_FAILURE) //검색 인풋 
 
 'STEP - [x] 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_search_clear'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
-
-'기대결과 - 검색어 삭제되고, 최근 검색어 노출'
-Mobile.verifyElementText(findTestObject('08_search/txt_recent_list'), search_list, FailureHandling.CONTINUE_ON_FAILURE) //검색어
 
 'STEP - [x] 최근 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_recent_list_delete'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
@@ -181,44 +168,42 @@ Mobile.verifyElementVisible(findTestObject('08_search/txt_hospital_name'), Globa
 Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_department'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //대표진료과목 영역
 
 '검색 결과 첫번째 병원 이름 저장'
-hospital_name3 = Mobile.getText(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+hospital_name = Mobile.getText(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
 'STEP - 병원 목록 선택'
 Mobile.tap(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-//병원상세 요소 못찾아서 보류
-//'기대결과 - 병원상세 화면으로 이동'
-//Mobile.verifyElementVisible(findTestObject('00_common/txt_class_text_view', [('text') : hospital_name3]), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //page title
-//
-//'STEP - [<-] 병원상세 뒤로가기 버튼 선택'
-//Mobile.tap(findTestObject('00_common/btn_webview_back'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+Mobile.delay(2)
+
+'STEP - 이전 화면으로 이동'
 Mobile.pressBack()
 
-'기대결과 - 검색 화면으로 이동'
+Mobile.delay(2)
+
+'기대결과 - 검색 화면으로 이동된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/input_search'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
 'STEP - [지도보기] 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_map'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 병원 지도 화면으로 이동'
+Mobile.delay(2)
+
+'기대결과 - 병원 지도 화면으로 이동된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/area_naver_map'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //네이버 지도
 Mobile.verifyElementVisible(findTestObject('08_search/area_hospital_map_list'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //바텀시트 영역
-Mobile.verifyElementVisible(findTestObject('08_search/txt_map_hospital_name', [('text') : hospital_name3]), GlobalVariable.fixedTime) //검색 목록 첫번째 병원이름
+Mobile.verifyElementVisible(findTestObject('08_search/txt_map_hospital_name', [('text') : hospital_name]), GlobalVariable.fixedTime) //검색 목록 첫번째 병원이름
 
 'STEP - 병원 선택'
 Mobile.tap(findTestObject('08_search/area_hospital_map_list'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-//병원상세 요소 못찾아서 보류
-//'기대결과 - 병원상세 화면으로 이동'
-//Mobile.verifyElementVisible(findTestObject('00_common/txt_class_text_view', [('text') : hospital_name]), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
-//
-//'STEP - [<-] 병원상세 뒤로가기 버튼 선택'
-//Mobile.tap(findTestObject('00_common/btn_webview_back'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+Mobile.delay(2)
 
-'back key'
+'STEP - 이전 화면으로 이동'
 Mobile.pressBack()
 
-'기대결과 - 병원 지도 화면으로 이동'
+Mobile.delay(2)
+
+'기대결과 - 병원 지도 화면으로 이동된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/area_naver_map'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //네이버 지도
 
 'STEP - [<-] 뒤로가기 버튼 선택'
@@ -232,13 +217,13 @@ Mobile.verifyElementText(findTestObject('08_search/input_search'), medical_depar
 'STEP - [x] 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_search_clear'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
-'기대결과 - 검색어 삭제되고, 최근 검색어 노출'
+'기대결과 - 검색어 삭제되고  최근 검색어가 노출된다.'
 Mobile.verifyElementText(findTestObject('08_search/txt_recent_list'), medical_department[i], FailureHandling.CONTINUE_ON_FAILURE) //검색어
 	
 'STEP - [x] 최근 검색어 삭제 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_recent_list_delete'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 삭제한 최근 검색어 미노출'
+'기대결과 - 삭제한 최근 검색어가 노출되지 않는다.'
 Mobile.verifyElementNotVisible(findTestObject('08_search/txt_recent_list'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 }
 //
@@ -247,12 +232,13 @@ Mobile.verifyElementNotVisible(findTestObject('08_search/txt_recent_list'), Glob
 'STEP - [<-] 뒤로가기 버튼 선택'
 Mobile.tap(findTestObject('00_common/btn_backBtn'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 홈 화면으로 이동'
-Mobile.verifyElementVisible(findTestObject('03_home/btn_menu_home_selected'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //홈 메뉴(활성화)
-Mobile.verifyElementVisible(findTestObject('03_home/area_view_search_bar'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //검색 바
+Mobile.delay(2)
+
+'기대결과 - 홈 화면으로 이동된다.'
+Mobile.verifyElementVisible(findTestObject('03_home/btn_menu_home_selected'), 10, FailureHandling.CONTINUE_ON_FAILURE) //홈 메뉴(활성화)
 //
 
-'최근앱 삭제'
-AndroidDriver<MobileElement> driver = MobileDriverFactory.getDriver()
-driver.pressKey(new KeyEvent(AndroidKey.APP_SWITCH))
-Mobile.tap(findTestObject('00_data_delete/btn_all_delete'), 2, FailureHandling.CONTINUE_ON_FAILURE)
+'STEP - 앱 종료'
+AppiumDriver<?> driver = MobileDriverFactory.getDriver()
+driver.terminateApp('com.bbros.sayup.debug')
+
--- Scripts/Sanity_TC/Scenario_05 검색/02_search_adress_edit/Script1686619954677.groovy
+++ Scripts/Sanity_TC/Scenario_05 검색/02_search_adress_edit/Script1686619954677.groovy
@@ -29,11 +29,13 @@ Mobile.startExistingApplication(GlobalVariable.appid)
 Mobile.delay(2)
 
 //검색
-'현재 설정되어있는 주소 이름'
+'STEP - 현재 설정되어있는 주소 이름 저장'
 home_adress_name = Mobile.getText(findTestObject('03_home/txt_home_adress_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
 'STEP - 홈 검색 영역 선택'
 Mobile.tap(findTestObject('03_home/area_view_search_bar'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
+
+Mobile.delay(2)
 
 'STEP - 진료과 검색어 입력'
 Mobile.setText(findTestObject('08_search/input_search'), '소아청소년과', GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
@@ -44,13 +46,13 @@ Mobile.tap(findTestObject('08_search/btn_search'), GlobalVariable.fixedTime, Fai
 'STEP - 검색 결과 첫번째 리스트 선택'
 Mobile.tap(findTestObject('08_search/btn_result_plus'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
-'기대결과 - 검색 영역에 검색어 노출'
+'기대결과 - 검색 영역에 검색어가 노출된다.'
 Mobile.verifyElementText(findTestObject('08_search/input_search'), '소아청소년과', FailureHandling.CONTINUE_ON_FAILURE)
 
 'STEP - sort 필터 선택'
 Mobile.tap(findTestObject('08_search/txt_sort_type'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 정렬필터 바텀 시트 노출'
+'기대결과 - 바텀시트가 노출되다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_sort_filter_title'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //타이틀
 Mobile.verifyElementText(findTestObject('00_common/btn_confirmBtn'), '필터 적용', FailureHandling.CONTINUE_ON_FAILURE) //[필터 적용] 버튼
 
@@ -60,10 +62,10 @@ Mobile.tap(findTestObject('08_search/txt_sort_filter_distance'), GlobalVariable.
 'STEP - [필터 적용] 버튼 선택'
 Mobile.tap(findTestObject('00_common/btn_confirmBtn'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - sort 필터에 {거리순} 노출'
+'기대결과 - 거리순으로 필터가 적용된다.'
 Mobile.verifyElementText(findTestObject('08_search/txt_sort_type'), '거리순', FailureHandling.CONTINUE_ON_FAILURE)
 	
-'기대결과 - 진료과 검색 결과 노출'
+'기대결과 - 진료과 검색 결과가 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //병원명 영역
 Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_department'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //대표진료과목 영역
 //
@@ -72,13 +74,15 @@ Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_departmen
 'STEP - 주소 버튼 선택'
 Mobile.tap(findTestObject('08_search/btn_searchRegionBtn'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 주소 설정 화면으로 이동'
+Mobile.delay(2)
+
+'기대결과 - 주소 설정 화면으로 이동된다.'
 Mobile.verifyElementText(findTestObject('00_common/txt_titleTxt'), '주소 설정', FailureHandling.CONTINUE_ON_FAILURE)
 
 'STEP - [현위치에서 병원 찾기] 버튼 선택'
 Mobile.tap(findTestObject('06_adress_setting/btn_current_location'), GlobalVariable.fixedTime)
 
-'기대결과 - 검색 화면 주소 이름 현위치 노출'
+'기대결과 - 검색 화면 주소 이름 현위치가 노출된다.'
 if(GlobalVariable.adress_name == '상동')
 {
 	Mobile.verifyElementVisible(findTestObject('08_search/txt_search_adress_name', [('text') : '상동']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
@@ -88,7 +92,7 @@ else if(GlobalVariable.adress_name == '여의도동')
 	Mobile.verifyElementVisible(findTestObject('08_search/txt_search_adress_name', [('text') : '여의도동']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 }
 
-'기대결과 - 검색 결과 노출'
+'기대결과 - 검색 결과가 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //병원명 영역
 Mobile.verifyElementVisible(findTestObject('08_search/area_complete_date'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //진료시간 영역
 Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_department'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //대표진료과목 영역
@@ -103,16 +107,16 @@ else if(GlobalVariable.adress_name == '여의도동')
 	Mobile.tap(findTestObject('08_search/txt_search_adress_name', [('text') : '여의도동']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 }
 
-'기대결과 - 주소 설정 화면으로 이동'
+'기대결과 - 주소 설정 화면으로 이동된다.'
 Mobile.verifyElementText(findTestObject('00_common/txt_titleTxt'), '주소 설정', FailureHandling.CONTINUE_ON_FAILURE)
 	
 'STEP - [우리집] 선택'
 Mobile.tap(findTestObject('06_adress_setting/btn_my_home'), GlobalVariable.fixedTime)
 
-'기대결과 - 검색 화면 주소 이름 우리집 노출'
+'기대결과 - 검색 화면 주소 이름에 우리집이 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_search_adress_name', [('text') : '우리집']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
-'기대결과 - 검색 결과 노출'
+'기대결과 - 검색 결과가 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //병원명 영역
 Mobile.verifyElementVisible(findTestObject('08_search/area_complete_date'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //진료시간 영역
 Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_department'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //대표진료과목 영역
@@ -120,10 +124,10 @@ Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_departmen
 'STEP - 주소 이름 선택'
 Mobile.tap(findTestObject('08_search/txt_search_adress_name', [('text') : '우리집']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 
-'기대결과 - 주소 설정 화면으로 이동'
+'기대결과 - 주소 설정 화면으로 이동된다.'
 Mobile.verifyElementText(findTestObject('00_common/txt_titleTxt'), '주소 설정', FailureHandling.CONTINUE_ON_FAILURE)
 
-'그 외 주소 없는 경우 추가'
+'STEP - 추가된 주소가 없는 경우 주소 추가'
 if(Mobile.waitForElementNotPresent(findTestObject('06_adress_setting/btn_different_adress_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE))
 {
 	'STEP - [다른 위치에서 병원 찾기] 버튼 선택'
@@ -141,7 +145,7 @@ if(Mobile.waitForElementNotPresent(findTestObject('06_adress_setting/btn_differe
 	'STEP - 도로명 주소 선택'
 	Mobile.tap(findTestObject('06_adress_setting/txt_adress_road_name'), GlobalVariable.fixedTime)
 	
-	'기대결과 - 상세주소 입력 타이틀 노출'
+	'기대결과 - 상세주소 입력 타이틀이 노출된다.'
 	Mobile.verifyElementText(findTestObject('00_common/txt_titleTxt'), '상세주소 입력', FailureHandling.CONTINUE_ON_FAILURE)
 	
 	'STEP - 상세주소 입력'
@@ -157,16 +161,15 @@ if(Mobile.waitForElementNotPresent(findTestObject('06_adress_setting/btn_differe
 'STEP - 주소이름 {TEST} 선택'
 Mobile.tap(findTestObject('06_adress_setting/btn_different_adress_name'), GlobalVariable.fixedTime)
 
-'기대결과 - 검색 화면 주소 이름 {TEST} 노출'
+'기대결과 - 검색 화면에 {TEST} 주소 이름이 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_search_adress_name', [('text') : 'TEST']), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE)
 	
-'기대결과 - 검색 결과 노출'
+'기대결과 - 검색 결과가 노출된다.'
 Mobile.verifyElementVisible(findTestObject('08_search/txt_hospital_name'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //병원명 영역
 Mobile.verifyElementVisible(findTestObject('08_search/area_complete_date'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //진료시간 영역
 Mobile.verifyElementVisible(findTestObject('08_search/txt_main_medical_department'), GlobalVariable.fixedTime, FailureHandling.CONTINUE_ON_FAILURE) //대표진료과목 영역
 //
 
-'최근앱 삭제'
-AndroidDriver<MobileElement> driver = MobileDriverFactory.getDriver()
-driver.pressKey(new KeyEvent(AndroidKey.APP_SWITCH))
-Mobile.tap(findTestObject('00_data_delete/btn_all_delete'), 2, FailureHandling.CONTINUE_ON_FAILURE)
+'STEP - 앱 종료'
+AppiumDriver<?> driver = MobileDriverFactory.getDriver()
+driver.terminateApp('com.bbros.sayup.debug')
